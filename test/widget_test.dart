426b68879684868ca5a0410b1718d4120226f9b6
omar8157 <mrm548500@gmail.com>
2026-03-27 23:10:27 +0200

    download assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:marketi/app/app.dart';
 
-import 'package:marketi/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    var myApp = const MyApp();
+    await tester.pumpWidget(myApp);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
